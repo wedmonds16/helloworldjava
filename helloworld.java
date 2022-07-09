8dd0b3c756cb295eb1ac0e7f37fd0b41b6429381
wedmonds16 <108109435+wedmonds16@users.noreply.github.com>
2022-07-09 19:49:34 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 9th jul 6:42 PM");
+    System.out.println("Hello World 9th jul 6:42 PM triggered by webhook");
     }
 }
